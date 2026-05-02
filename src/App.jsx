ee309738d4ff2fd22bd8bfa061a9ec61238c309e
Fleabyte26 <krishornung@gmail.com>
2026-05-02 12:57:10 -0400

    Integrated Instant Consultation component and updated routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import Navbar from "./Components/Navbar/Navbar";
 import Landing_Page from "./Components/Landing_Page/Landing_Page";
 import Login from "./Components/Login/Login";
 import Sign_Up from "./Components/Sign_Up/Sign_Up";
-
-// Instant Consultation Booking component
 import InstantConsultation from "./Components/InstantConsultationBooking/InstantConsultation";
 
 function App() {
@@ -14,26 +12,13 @@ function App() {
     <div className="App">
       <BrowserRouter>
 
-        {/* Navbar visible on all pages */}
         <Navbar />
 
         <Routes>
-
-          {/* Home page */}
           <Route path="/" element={<Landing_Page />} />
-
-          {/* Login page */}
           <Route path="/login" element={<Login />} />
-
-          {/* Sign Up page */}
           <Route path="/signup" element={<Sign_Up />} />
-
-          {/* Instant Consultation Booking page */}
-          <Route
-            path="/instant-consultation"
-            element={<InstantConsultation />}
-          />
-
+          <Route path="/instant-consultation" element={<InstantConsultation />} />
         </Routes>
 
       </BrowserRouter>
