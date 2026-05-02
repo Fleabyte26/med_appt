c3614f1be5b583b737ee25f3bd53b2ebee40b451
Fleabyte26 <krishornung@gmail.com>
2026-05-02 10:08:38 -0400

    Updated MongoDB connection in db.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:ospg573q7xPQojk5uPEXH2dx@172.21.121.154:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
